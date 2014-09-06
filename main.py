a8f7b69b2a11b01de6f7c71bb955095fa0581a3b
Thomas <thomas.dewachter@student.kuleuven.be>
2014-09-06 19:27:47 +0200

    Added convergence plots & calculates max possible size

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#Thomas
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +22,9 @@ def search_polygon(polygon, nb_iterations):
 	"""
 
 	max_polygon = polygon
-	max_scale = 0
+	max_score = 0
+	
+	errors = np.zeros((2, nb_iterations - 1))
 
 	#iterates for set number of loops
 	for i in range(1, nb_iterations):
@@ -32,42 +33,80 @@ def search_polygon(polygon, nb_iterations):
 		x = np.random.uniform(0, 1)
 		y = np.random.uniform(0, 1)
 		angle = np.random.uniform(0,2*np.pi)
-		scale = np.random.uniform(0, 0.2)
+		#scale = np.random.uniform(0, get_possible_scale(polygon, x, y, angle))
+		scale = get_possible_scale(polygon, x, y, angle)
 
 		#transform the base polygon according to configuration
 		verts = scale * rotate_polygon(polygon, angle, 0, 0) + np.array([x,y])
+		
+		#calculate score using objective function
+		score = get_score(x, y, angle, scale)
+		errors[0,i-1] = i
+		errors[1,i-1] = 0.5528 - max_score #0.7071 - max_score
 
 		#make checks and adjust max if necessary
-		if (verts > 0).all() and (verts < 1).all() and scale > max_scale:
+		if score > max_score and (verts < 1).all() and (verts > 0).all():
 				max_polygon = verts
-				max_scale = scale
+				max_score = score
 
-	print verts, (verts > 0).all(), (verts < 1).all()
-	print scale, max_scale   
-	return max_polygon
+	return max_polygon, errors
+
+def get_possible_scale(polygon, x, y, angle):
+	""" Calculates the maximum possible scale given x, y and angle
+	
+	"""
+	
+	verts = rotate_polygon(polygon, angle, 0, 0)
+	assert (verts != 0).all()
+	
+	scales = verts
+	for s in scales:
+		if s[0] < x:
+			s[0] = -x/s[0]
+		else:
+			s[0] = (1-x)/s[0]
+			
+		if s[1] < y:
+			s[1] = -y/s[1]
+		else:
+			s[1] = (1-y)/s[1]
+	
+	return np.min(scales)
+	
 
 
-def plot_polygon(verts):
+def get_score(x, y, angle, scale):
+	""" Calculates score using objective function
+	
+	"""
+	
+	return scale
+
+
+def plot_polygon(verts, errors):
 	""" Plots a polygon (square) with center (x,y)
 	
 	"""
-
-	codes = [	Path.MOVETO,
-				Path.LINETO,
-				Path.LINETO,
-				Path.LINETO,
-				Path.CLOSEPOLY,
-				]
+	
+	#Configure polygon
+	codes =  [Path.LINETO] * (verts.shape[0] - 1)
+	codes[0] = Path.MOVETO
+	codes.append(Path.CLOSEPOLY)
 
 	path = Path(verts, codes)
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
+	#fig = plt.figure()
+	ax = plt.subplot(211)
 	patch = patches.PathPatch(path, facecolor='orange', lw=2)
 	ax.add_patch(patch)
 	plt.xlim(0,1)
 	plt.ylim(0,1)
 	plt.gca().set_aspect('equal', adjustable='box')
+	
+	plt.subplot(212)
+	plt.loglog(errors[0], errors[1])
+	plt.title('Convergence')
+	
 	plt.show()
 
 
@@ -75,21 +114,18 @@ def define_polygon(number_sides):
 	""" Returns an array with the coordinates of a regular polygon with radius 1
 
 	"""
+	assert number_sides >= 3
 	
-	result = []
 	angle = 2*np.pi/number_sides
-
-	#iterate over number of sides
-	for n in range(0,number_sides + 1):
-		result.append((np.cos(n*angle), np.sin(n*angle)))
+	result = np.array( [(np.cos(n*angle), np.sin(n*angle)) for n in range(0, number_sides + 1)] )
 
 	return result
 
 
 def main():
-	square = define_polygon(4)
-	max_square = search_polygon(square, 1000)
-	plot_polygon(max_square)
+	square = define_polygon(5)
+	[max_square, errors] = search_polygon(square, 1000000)
+	plot_polygon(max_square, errors)
 
 if __name__ == '__main__':
 	main()
